3303bae0a4954f4f95369ed2039bc6f143776fb5
Peso11 <answer0333@gmail.com>
2018-02-27 02:17:48 +0300

    rdy

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+var xx =1;
+var numberOfZeros=0;
+do { numberOfZeros += Math.floor(number/Math.pow(5, xx));
+xx++;
+}
+while (Math.pow(5, xx) <=number );
+
+  return numberOfZeros;
 }
